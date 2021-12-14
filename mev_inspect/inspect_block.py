fd5920c0a9d5ce696d04842523112e8df3253474
Luke Van Seters <lukevanseters@gmail.com>
2021-12-13 20:06:25 -0500

    Add logic and writing for sandwiches. Add tests too

--- mev_inspect/inspect_block.py
+++ mev_inspect/inspect_block.py
@@ -7,44 +7,37 @@ from web3 import Web3
 from mev_inspect.arbitrages import get_arbitrages
 from mev_inspect.block import create_from_block_number
 from mev_inspect.classifiers.trace import TraceClassifier
-from mev_inspect.crud.arbitrages import (
-    delete_arbitrages_for_block,
-    write_arbitrages,
-)
-
-from mev_inspect.crud.punks import (
-    delete_punk_snipes_for_block,
-    write_punk_snipes,
-    delete_punk_bids_for_block,
-    write_punk_bids,
-    delete_punk_bid_acceptances_for_block,
-    write_punk_bid_acceptances,
-)
-
-from mev_inspect.crud.blocks import (
-    delete_block,
-    write_block,
-)
-from mev_inspect.crud.traces import (
-    delete_classified_traces_for_block,
-    write_classified_traces,
+from mev_inspect.crud.arbitrages import delete_arbitrages_for_block, write_arbitrages
+from mev_inspect.crud.blocks import delete_block, write_block
+from mev_inspect.crud.liquidations import (
+    delete_liquidations_for_block,
+    write_liquidations,
 )
 from mev_inspect.crud.miner_payments import (
     delete_miner_payments_for_block,
     write_miner_payments,
 )
-
-from mev_inspect.crud.swaps import delete_swaps_for_block, write_swaps
-from mev_inspect.crud.transfers import delete_transfers_for_block, write_transfers
-from mev_inspect.crud.liquidations import (
-    delete_liquidations_for_block,
-    write_liquidations,
+from mev_inspect.crud.punks import (
+    delete_punk_bid_acceptances_for_block,
+    delete_punk_bids_for_block,
+    delete_punk_snipes_for_block,
+    write_punk_bid_acceptances,
+    write_punk_bids,
+    write_punk_snipes,
 )
+from mev_inspect.crud.sandwiches import delete_sandwiches_for_block, write_sandwiches
+from mev_inspect.crud.swaps import delete_swaps_for_block, write_swaps
+from mev_inspect.crud.traces import (
+    delete_classified_traces_for_block,
+    write_classified_traces,
+)
+from mev_inspect.crud.transfers import delete_transfers_for_block, write_transfers
+from mev_inspect.liquidations import get_liquidations
 from mev_inspect.miner_payments import get_miner_payments
 from mev_inspect.punks import get_punk_bid_acceptances, get_punk_bids, get_punk_snipes
+from mev_inspect.sandwiches import get_sandwiches
 from mev_inspect.swaps import get_swaps
 from mev_inspect.transfers import get_transfers
-from mev_inspect.liquidations import get_liquidations
 
 
 logger = logging.getLogger(__name__)
@@ -108,6 +101,12 @@ async def inspect_block(
 
     delete_liquidations_for_block(inspect_db_session, block_number)
     write_liquidations(inspect_db_session, liquidations)
+
+    sandwiches = get_sandwiches(swaps)
+    logger.info(f"Block: {block_number} -- Found {len(sandwiches)} sandwiches")
+
+    delete_sandwiches_for_block(inspect_db_session, block_number)
+    write_sandwiches(inspect_db_session, sandwiches)
 
     punk_bids = get_punk_bids(classified_traces)
     delete_punk_bids_for_block(inspect_db_session, block_number)
